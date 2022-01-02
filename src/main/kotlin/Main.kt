95897289f879898801a3464cad5109633dc17523
Abu Sufian <abusufianshaon@gmail.com>
2022-01-02 22:27:22 +0600

    Take Studnet Info

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,14 +6,60 @@ fun main() {
     println("*********Student Info Management*********\n")
     print("Enter total student number: ")
     val n = scn.nextInt()
+    scn.nextLine()
 
     val studentArray = Array<Student?>(size = n) { null }
 
     for (i in studentArray.indices) {
-        val scn2 = Scanner(System.`in`)
         print("${i + 1}. Enter Student Name:")
-        val name = scn2.nextLine()
-        studentArray[i] = Student(name)
+        val name = scn.nextLine()
+
+        print("Enter Roll: ")
+        val roll = scn.nextInt()
+        scn.nextLine()
+
+        print("Enter Class Name: ")
+        val className = scn.nextLine()
+
+        print("Enter Marks(Bangla): ")
+        val banglaMarks = scn.nextDouble()
+
+        print("Enter Marks(English): ")
+        val englishMarks = scn.nextDouble()
+
+        print("Enter Marks(Math): ")
+        val mathMarks = scn.nextDouble()
+        scn.nextLine()
+
+        val bangla = Subject(
+            name = "Bangla",
+            code = 111,
+            totalMarks = 100.0,
+            earned = banglaMarks
+        )
+
+        val english = Subject(
+            code = 112,
+            name = "English",
+            totalMarks = 100.0,
+            earned = englishMarks
+        )
+
+        val math = Subject(
+            code = 113,
+            name = "Math",
+            totalMarks = 100.0,
+            earned = mathMarks
+        )
+
+        studentArray[i] = Student(
+            name = name,
+            className = className,
+            bangla = bangla,
+            english = english,
+            math = math,
+            roll = roll,
+        )
     }
 
     println("\nEntered Student Name:")
@@ -22,6 +68,4 @@ fun main() {
             println(s.name)
         }
     }
-
-
 }
--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,13 +1,11 @@
 class Student(
-    var name:String,
-
-){
-
-//    private var roll:Int,
-//    private var className:String,
-//    private var bangla:Subject,
-//    private var english:Subject,
-//    private var math:Subject
+    var name: String,
+    var roll: Int,
+    var className: String,
+    var bangla: Subject,
+    var english: Subject,
+    var math: Subject
+) {
 
 
 }
